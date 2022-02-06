d6c8fcb92e438cdfdee0bec4662457127041c575
MisaoDev <69985862+MisaoDev@users.noreply.github.com>
2022-02-05 23:35:28 -0300

    implement basic functionality

--- src/component/Note/Note.js
+++ src/component/Note/Note.js
@@ -1,57 +1,40 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import './Note.scss';
 
 function Note(props) {
-  const {id} = props;
-  const defaultText = {
-    title  : props.title   || 'title',
-    content: props.content || 'content',
-  };
-
-  const editableRef = {
-    title: useRef('New note'),
-    content: useRef(''),
-  }
-
-  function handleInput(event) {
-    const name = event.target.getAttribute('name');
-    const value = event.target.textContent;
-    props.onChange(id, {[name]: value});
-
-    // editableRef[name].current.textContent = value;
-  }
+  const { id, title, content, color } = props;
 
   const noteStyle = {
-    width: 400,
+    width: '400px',
+    backgroundColor: color || '#8AF9FF',
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    props.onChange(
+      id,
+      { [name]: value }
+    );
   }
 
   return (
-    <div className="note card has-background-info" style={noteStyle}>
-      <header className="">
-        <p
-          className="card-header-title"
-          contentEditable
-          suppressContentEditableWarning={true}
-          name="title"
-          ref={editableRef.title}
-          onInput={handleInput}
-        >
-          {defaultText.title}
-        </p>
-      </header>
-      <div className="card-content">
-        <div
-          className="content"
-          contentEditable
-          suppressContentEditableWarning={true}
-          name="content"
-          ref={editableRef.content}
-          onInput={handleInput}
-        >
-          {defaultText.content}
-        </div>
-      </div>
+    <div className="note" style={noteStyle}>
+      <input
+        type="text"
+        className="note-title input"
+        name="title"
+        value={title}
+        onChange={handleChange}
+      />
+      <textarea
+        type="text"
+        className="note-content input"
+        name="content"
+        value={content}
+        onChange={handleChange}
+      />
     </div>
   );
-};
+}
+
 export default Note;
